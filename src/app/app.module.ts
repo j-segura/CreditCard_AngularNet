e13cadb8d34b3e61453b7ad8f64b47f635c536ec
j-segura <juanda.segura2005@gmail.com>
2023-06-28 11:30:50 -0500

    mock-up ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CreditCardComponent } from './components/credit-card/credit-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreditCardComponent
   ],
   imports: [
     BrowserModule
